7716b3c84d7ed456ea1a26d94d884e1048a4e13c
tombo314 <u_tombo@icloud.com>
2023-09-26 09:33:34 +0900

    Update outのファイルを作る

--- sample.py
+++ sample.py
@@ -10,3 +10,6 @@ with open(f"in/in_{nodes_num}.txt", "w") as f:
     f.write(f"{nodes_num}\n")
     for i in range(nodes_num):
         f.write(f"{nodes_x[i]} 0\n")
+
+with open(f"out/out_{nodes_num}.txt", "w") as f:
+    f.write(f"{(nodes_num-1)*2}\n")
